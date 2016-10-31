912d016e34d583fb25f5607cbad25dad2402fb80
Darah <darahfumadac@gmail.com>
2016-10-31 20:51:48 +0800

    Adding Manage class form.cs

---
+++ MyClassRecord/Views/ManageClassForm.cs
@@ -0,0 +1,10 @@
+﻿namespace MyClassRecord.Views
+{
+    public partial class ManageClassForm : ManagerForm
+    {
+        public ManageClassForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
